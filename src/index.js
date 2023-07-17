cfd5b5c7662e3ee2d9f52b0509f2eba8d232d68d
Ilia Bayanov <i.bajanov@itorum.ru>
2023-07-18 00:43:21 +0300

    feat: process.on exit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import { httpServer } from './http_server/index.js';
+import { wsServer } from './ws_server';
 
 const HTTP_PORT = 8181;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
+
+process.on('exit', () => wsServer.close());
